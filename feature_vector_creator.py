417dbe5dd76586fa7cbb056f468f1a22fb0700ee
Giyoung Kim <gk122@duke.edu>
2024-04-04 17:26:00 -0400

    Fixed merging data on Date

--- feature_vector_creator.py
+++ feature_vector_creator.py
@@ -7,11 +7,15 @@ def feature_vector():
     acled_data = pd.read_csv("data/events_over_time.csv")
     trends_data = pd.read_csv("data/trends_data.csv")
 
+    stock_data["Date"] = pd.to_datetime(stock_data["Date"])
+    acled_data["Date"] = pd.to_datetime(acled_data["Date"])
+    trends_data["Date"] = pd.to_datetime(trends_data["Date"])
+
     # Merge the dataframes on the "Date" column
     merged_data = pd.merge(stock_data, acled_data, on="Date", how="left")
     merged_data = pd.merge(merged_data, trends_data, on="Date", how="left")
 
     # Save the merged dataframe to a new CSV file if needed
-    merged_data.to_csv("merged_data.csv", index=False)
+    merged_data.to_csv("data/merged_data.csv", index=False)
 
     return merged_data
