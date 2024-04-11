3e4db59fa45fbf4a5dbcb27ba3a3392c93332629
Giyoung Kim <gk122@duke.edu>
2024-04-11 16:24:50 -0400

    Changed to logistic

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import warnings
 
 import pandas as pd
 from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
 
 from feature_vector_creator import feature_vector
 
@@ -9,8 +10,8 @@ from feature_vector_creator import feature_vector
 def calc_price_change(df):
     target_df = pd.DataFrame()
     target_df["Date"] = df["Date"]
-    target_df["Pos"] = (df["High Price"] - df["Open Price"]) / df["Open Price"] * 100
-    target_df["Neg"] = (df["Low Price"] - df["Open Price"]) / df["Open Price"] * 100
+    target_df["Long"] = (df["High Price"] - df["Open Price"]) / df["Open Price"] > 0.03
+    target_df["Short"] = (df["Low Price"] - df["Open Price"]) / df["Open Price"] < -0.03
 
     return target_df
 
@@ -21,21 +22,39 @@ def main():
 
     interval_length = 26
     feature_col = ["Open Price", "war", "conflict", "united states"]
-    warnings.filterwarnings("ignore",
-                            message="X does not have valid feature names, but LinearRegression was fitted with feature names")
+    warnings.filterwarnings(
+        "ignore",
+        message="X does not have valid feature names, but LogisticRegression was fitted with feature names"
+    )
+
+    n_classes = 2
+    conf_matrix = [[0] * n_classes for _ in range(n_classes)]
+
     for i in range(feature_df.shape[0]-interval_length-1):
         start = i
         end = i + interval_length + 1
 
         X_train = feature_df.iloc[start:end][feature_col]
         X_test = feature_df.iloc[end][feature_col].values.reshape(1, -1)
-        y_train = target_df.iloc[start:end]["Pos"]
-        y_test = target_df.iloc[end]["Pos"]
+        y_train = target_df.iloc[start:end]["Long"]
+        y_test = target_df.iloc[end]["Long"]
 
-        model = LinearRegression()
+        # model = LinearRegression()
+        model = LogisticRegression()
         model.fit(X_train, y_train)
-        print(f"Predicted: {model.predict(X_test)[0]}, Actual: {y_test}")
+        y_pred = model.predict(X_test)[0]
+        print(f"Predicted: {y_pred}, Actual: {y_test}")
 
+        y_test = int(y_test)
+        y_pred = int(y_pred)
+        conf_matrix[y_test][y_pred] += 1
+
+    print(f"conf matrix:\t{conf_matrix[0]}")
+    for i in range(1, n_classes):
+        print(f"\t\t{conf_matrix[i]}")
+
+    # TODO: Visualization
+    # TODO: Get rid of bad trades
 
 if __name__ == "__main__":
     main()
