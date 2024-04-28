7d260e5fa1853ab5a8a8352ddaefba94d4d7dc4f
Giyoung Kim <gk122@duke.edu>
2024-04-28 17:28:40 -0400

    Added blotter creation and analysis

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 
-from orders import get_orders
+from orders import create_blotter, get_orders
 from dash_app import dash_app
 from feature_vector_creator import calc_price_change, feature_vector
 from model import get_predictions
@@ -16,22 +16,28 @@ def main():
 
     # Predict whether to long
     interval_length = 13
+    first_date = feature_df.iloc[interval_length + 1]["Date"]
+    print(f"Starting at {first_date}")
     feature_col = ["Open Price", "war", "conflict", "united states", "Count", "FEDFUNDS"]
-    predictions = get_predictions(feature_df, target_df, feature_col, interval_length)
+    predictions = get_predictions(feature_df, target_df, feature_col, interval_length, True)
 
     # Get buy and sell orders based on predictions
     daily_df = pd.read_csv("data/refinitiv_palantir_daily.csv")
     daily_df["Date"] = pd.to_datetime(daily_df["Date"])
+    first_idx = daily_df[daily_df["Date"] == first_date].index[0]
+    daily_df = daily_df.iloc[first_idx:]
+    daily_df.reset_index(drop=True, inplace=True)
     buy_orders, sell_orders = get_orders(daily_df, predictions, perc_change)
 
     # TODO: Create blotter
+    blotter_df = create_blotter(buy_orders, sell_orders, first_date, True)
 
     # TODO: Analyze blotter
 
     # TODO: Get rid of bad trades
 
     # Visualization
-    dash_app(daily_df, buy_orders, sell_orders)
+    # dash_app(daily_df, buy_orders, sell_orders)
 
 
 if __name__ == "__main__":
--- orders.py
+++ orders.py
@@ -4,6 +4,67 @@ import numpy as np
 import pandas as pd
 
 
+def create_blotter(buy_orders: list,
+                   sell_orders: list,
+                   first_date: datetime.timestamp,
+                   print_stats: bool = False):
+    blotter_df = pd.DataFrame(columns=["Identifier",
+                                       "EntryTimestamp",
+                                       "ExitTimestamp",
+                                       "TradeQuantity",
+                                       "Price",
+                                       "EntryPrice",
+                                       "ExitPrice",
+                                       ])
+    for i in range(len(buy_orders)):
+        row = {"Identifier": "PLTR.K",
+               "EntryTimestamp": buy_orders[i][0],
+               "ExitTimestamp": sell_orders[i][0],
+               "Quantity": 1,
+               "EntryPrice": buy_orders[i][1],
+               "ExitPrice": sell_orders[i][1],
+               }
+        blotter_df = pd.concat([blotter_df, pd.DataFrame([row])], ignore_index=True)
+
+    # Extra Stats
+    blotter_df["Days"] = np.maximum(1, (blotter_df["ExitTimestamp"] - blotter_df["EntryTimestamp"]).dt.days)
+    blotter_df["Return"] = np.log(blotter_df["ExitPrice"] / blotter_df["EntryPrice"]) * np.sign(blotter_df["Quantity"])
+    blotter_df["DailyReturn"] = blotter_df["Return"] / blotter_df["Days"]
+    blotter_df["Value"] = abs(blotter_df["Quantity"] * blotter_df["EntryPrice"])
+    matched_df = blotter_df[pd.notna(blotter_df["ExitTimestamp"])]
+    portfolio_value = np.sum(matched_df["Value"])
+    grouped_df = matched_df.groupby("Identifier")
+
+    # Strategy Expected Returns
+    expected_returns = {}
+    weights = {}
+    for name, group in grouped_df:
+        identifier = matched_df[matched_df["Identifier"] == name]
+        expected_returns[name] = np.prod(1 + identifier["DailyReturn"]) ** (1 / len(identifier)) - 1
+        weights[name] = np.sum(identifier["Value"]) / portfolio_value
+    expected_portfolio_returns = np.sum([x * y for x, y in zip(weights.values(), expected_returns.values())])
+    if print_stats:
+        print(f"Strategy's Expected Returns: {round(100 * expected_portfolio_returns, 4)}%")
+
+    # Volatility
+    portfolio_volatility = np.std(matched_df["DailyReturn"])
+    if print_stats:
+        print(f"Strategy's Volatility: {round(portfolio_volatility, 4)}")
+
+    # Risk Free Rate
+    # https://home.treasury.gov/resource-center/data-chart-center/interest-rates/TextView?type=daily_treasury_yield_curve&field_tdr_date_value=2023
+    risk_free_rate = 0.09 / 30 / 100
+    if print_stats:
+        print(f"Daily Risk Free Rate: {round(100 * risk_free_rate, 4)}%")
+        print(f"\t(based on {first_date.date()})")
+
+    # Sharpe Ratio
+    sharpe = (expected_portfolio_returns - risk_free_rate) / portfolio_volatility
+    if print_stats:
+        print(f"Strategy's Sharpe Ratio: {round(sharpe, 4)}")
+
+    return blotter_df
+
 def get_orders(df: pd.DataFrame, predictions: dict, perc_change: float):
     buy_orders = []
     sell_orders = []
